149f9a39cce0bdf7b79ea4495fc4690df808ca5f
Lucas Knudsen <laksteelhouse@gmail.com>
2021-04-06 17:29:50 +0200

    Reverting back to working state

--- README.md
+++ README.md
@@ -5,3 +5,67 @@
 * Submit
 * Receives an image
 * Show this image
+
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+import { GLView } from 'expo-gl';
+import { THREE } from 'expo-three';
+import ExpoTHREE from 'expo-three';
+
+const ObjectReceiver = () => {
+
+  const _onGLContextCreate = async gl => {
+    scene = new THREE.Scene();
+    camera = new THREE.Camera(
+      75,
+      gl.drawingBufferWidth / gl.drawingBufferHeight,
+      0.1,
+      1000
+    );
+    scene.add(camera);
+    renderer = new ExpoTHREE.Renderer({ gl });
+    geometry = new THREE.BoxGeometry(1, 1, 1);
+    material = new THREE.MeshBasicMaterial({ color: 0xff0f00 });
+    obj = new THREE.Mesh(geometry, material);
+    edges = new THREE.EdgesGeometry(geometry);
+    line = new THREE.LineSegments(
+      edges,
+      new THREE.LineBasicMaterial({ color: 0xffffff })
+    );
+
+    scene.add(line);
+    scene.add(obj);
+    animate();
+  };
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    //geometry.rotation.x += 0.01;
+    //geometry.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  };
+
+
+  return (
+    <View style={styles.container}>
+      <GLView
+        style={{ width: 300, height: 300 }}
+        onContextCreate={_onGLContextCreate}
+      />
+    </View>
+  );
+}
+
+export default ObjectReceiver
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+});
--- components/ObjectReceiver.jsx
+++ components/ObjectReceiver.jsx
@@ -1,40 +1,77 @@
 import React, { useState, useEffect } from 'react'
 import { globals } from '../styles/globals'
-import { StyleSheet, Text, View } from 'react-native'
-import { Renderer } from 'expo-three';
-import { THREE } from 'expo-three';
-import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
+import { StyleSheet, TouchableWithoutFeedback, Text, View } from 'react-native'
 
-global.THREE = global.THREE || THREE;
+
 
 const API_KEY_3D = 'jcDuLnzAQdVXUJkuuhCojttASWcfaWlaewGkWwg'
 
 const ObjectReceiver = ({ route, navigation }) => {
-  const { height, photo } = route.params.data
+  // const { height, photo } = route.params.data
   const [objectImage, setObjectImage] = useState()
+
+
 
   useEffect(() => {
 
   }, [])
 
-  const fetch3DImage = (height, photo) => {
-    let body = {
-      height: height,
-      imageurl: photo.uri
-    }
-  }
+  // const fetch3DImage = (height, photo) => {
+  //   let body = {
+  //     height: height,
+  //     imageurl: photo.uri
+  //   }
+  // }
 
   return (
     <View style={globals.container}>
       <View style={styles.formContainer}>
-        <GLView
+
+        {/* <ModelView
+          model={{
+            uri: '../assets/avatar.obj',
+          }}
+          scale={0.10}
+
+          translateZ={-2}
+          rotateZ={270}
+
           style={{ flex: 1 }}
-          onContextCreate={({ gl: ExpoWebGLRenderingContext }) => {
+        /> */}
+
+        {/* <GLView
+          style={{ flex: 1 }}
+          onContextCreate={async (gl) => {
+            const { drawingBufferWidth: width, drawingBufferHeight: height } = gl;
             // Create a WebGLRenderer without a DOM element
             const renderer = new Renderer({ gl });
-            renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+            renderer.setSize(width, height);
+            renderer.setClearColor("#fff");
+
+            // Scene declaration
+            const scene = new THREE.Scene()
+
+
+
+            // Loading in the object
+            const asset = Asset.fromModule(require('../assets/avatar.obj'));
+            await asset.downloadAsync();
+            const loader = new OBJLoader();
+            loader.load(asset.localUri, group => {
+              scene.add(group)
+            });
+
+            // Rendering
+            const render = () => {
+              requestAnimationFrame(render);
+              renderer.render(scene, camera);
+              gl.endFrameEXP();
+            }
+            render()
           }}
-        />
+        /> */}
+
+        <Text>Are we here?</Text>
       </View>
     </View>
   )
