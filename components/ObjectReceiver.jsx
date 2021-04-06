7158b9bc7239de159e6a26ded11bc9f3b423aa69
Lucas Knudsen <laksteelhouse@gmail.com>
2021-04-06 13:38:21 +0200

    Version control

--- components/ObjectReceiver.jsx
+++ components/ObjectReceiver.jsx
@@ -1,19 +1,40 @@
 import React, { useState, useEffect } from 'react'
 import { globals } from '../styles/globals'
 import { StyleSheet, Text, View } from 'react-native'
-import { avatar } from '../assets/favicon.png'
+import { Renderer } from 'expo-three';
+import { THREE } from 'expo-three';
+import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
+
+global.THREE = global.THREE || THREE;
+
+const API_KEY_3D = 'jcDuLnzAQdVXUJkuuhCojttASWcfaWlaewGkWwg'
 
 const ObjectReceiver = ({ route, navigation }) => {
   const { height, photo } = route.params.data
+  const [objectImage, setObjectImage] = useState()
 
   useEffect(() => {
 
   }, [])
 
+  const fetch3DImage = (height, photo) => {
+    let body = {
+      height: height,
+      imageurl: photo.uri
+    }
+  }
+
   return (
     <View style={globals.container}>
       <View style={styles.formContainer}>
-
+        <GLView
+          style={{ flex: 1 }}
+          onContextCreate={({ gl: ExpoWebGLRenderingContext }) => {
+            // Create a WebGLRenderer without a DOM element
+            const renderer = new Renderer({ gl });
+            renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+          }}
+        />
       </View>
     </View>
   )
